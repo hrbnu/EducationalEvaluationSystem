1a949a9da2f52d05548814bb0e8ee4ce9662be35
iwts <1065077057@qq.com>
2018-12-07 18:21:49 +0800

    update 18.12.7-2
    删除idea一些配置

--- out/artifacts/educational_evaluation_system_war_exploded/src/main/java/edu/cs/hrbnu/DAO/ComplaintMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.cs.hrbnu.DAO;
-
-import edu.cs.hrbnu.model.Complaint;
-
-import java.util.List;
-
-public interface ComplaintMapper {
-    List<Complaint> getAllComplaint() throws Exception;
-}
--- out/artifacts/educational_evaluation_system_war_exploded/src/main/java/edu/cs/hrbnu/DAO/CourseMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.cs.hrbnu.DAO;
-
-import edu.cs.hrbnu.model.Course;
-
-import java.util.List;
-
-public interface CourseMapper {
-    Course getCourseById(String id) throws Exception;
-}
