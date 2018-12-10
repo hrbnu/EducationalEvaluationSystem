7c50dd77eeaa3008781ccc3d98e7955177ce5539
Yaque <yaquepeng@gmail.com>
2018-12-10 20:53:07 +0800

    change erro evaluateProblemMapper.xml file

--- src/main/java/edu/cs/hrbnu/controller/StudentController.java
+++ src/main/java/edu/cs/hrbnu/controller/StudentController.java
@@ -1,12 +1,16 @@
 package edu.cs.hrbnu.controller;
 
 import edu.cs.hrbnu.model.Course;
+import edu.cs.hrbnu.model.EvaluateProblem;
 import edu.cs.hrbnu.model.Student;
 import edu.cs.hrbnu.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student")
@@ -15,9 +19,10 @@ public class StudentController {
     private StudentService studentService;
 
     @RequestMapping("/eval")
-    public ModelAndView evaluation(Student student, Course course) {
-        studentService.evaluateCurrentCourse(student.getStudentId(), course.getCourseId());
-        return new ModelAndView("success");
+    public ModelAndView evaluation(Model model, Student student, Course course) {
+        List<EvaluateProblem> listEvaluateProblem = studentService.evaluateCurrentCourse(student.getStudentId(), course.getCourseId());
+        model.addAttribute("listEvaluateProblem", listEvaluateProblem);
+        return new ModelAndView("evalu/evalu");
     }
 
 }
