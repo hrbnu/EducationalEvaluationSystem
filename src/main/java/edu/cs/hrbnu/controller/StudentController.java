2524264eea0d1e946752a8ca2c8a6c7273d299a6
mayflying <1299822940@qq.com>
2018-12-10 23:47:07 +0800

    test

--- src/main/java/edu/cs/hrbnu/controller/StudentController.java
+++ src/main/java/edu/cs/hrbnu/controller/StudentController.java
@@ -25,12 +25,6 @@ public class StudentController {
         return new ModelAndView("evalu/evalu");
     }
 
-    @RequestMapping("/eval")
-    public ModelAndView evaluation1(Model model, Student student, Course course) {
-        List<EvaluateProblem> listEvaluateProblem = studentService.evaluateCurrentCourse(student.getStudentId(), course.getCourseId());
-        model.addAttribute("listEvaluateProblem", listEvaluateProblem);
-        return new ModelAndView("evalu/evalu");
-    }
 
 
 }
