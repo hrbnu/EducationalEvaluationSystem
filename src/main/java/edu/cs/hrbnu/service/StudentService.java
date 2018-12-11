9978fd759886248f68ae3a7afcbc3fd258f4e0a1
Yaque <yaquepeng@gmail.com>
2018-12-11 21:57:39 +0800

    evaluate view by jsp

--- src/main/java/edu/cs/hrbnu/service/StudentService.java
+++ src/main/java/edu/cs/hrbnu/service/StudentService.java
@@ -50,7 +50,7 @@ public interface StudentService {
 	/**
 	 * 评价当前课程
 	 */
-	void evaluateCurrentCourse(String studentId, String courseId);
+	void evaluateCurrentCourse(Student student, String courseId, double courseScores, String evaluateContent);
 	
 	/**
      * 投诉
