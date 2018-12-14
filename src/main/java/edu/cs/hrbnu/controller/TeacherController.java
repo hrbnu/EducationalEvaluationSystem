052b66f23f09e25e32a23219e1e5352f35d7e2a6
Yaque <yaquepeng@gmail.com>
2018-12-14 15:45:38 +0800

    yaque

--- src/main/java/edu/cs/hrbnu/controller/TeacherController.java
+++ src/main/java/edu/cs/hrbnu/controller/TeacherController.java
@@ -1,21 +1,28 @@
 package edu.cs.hrbnu.controller;
 
 
+import edu.cs.hrbnu.model.Evaluate;
+import edu.cs.hrbnu.model.EvaluateProblem;
 import edu.cs.hrbnu.DAO.CourseMapper;
 import edu.cs.hrbnu.DAO.EvaluateMapper;
 import edu.cs.hrbnu.model.Course;
 import edu.cs.hrbnu.model.Evaluate;
 import edu.cs.hrbnu.model.Teacher;
+import edu.cs.hrbnu.service.SystemService;
 import edu.cs.hrbnu.service.TeacherService;
 import edu.cs.hrbnu.uitl.ExcelUitl;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
@@ -26,7 +33,7 @@ import java.util.List;
 @RequestMapping("/teacher")
 public class TeacherController {
     @Autowired
-    TeacherService teacherService;
+    private TeacherService teacherService;
 
     @RequestMapping(value = "/reset")
     public ModelAndView reset(Teacher teacher){
@@ -55,54 +62,39 @@ public class TeacherController {
         return modelAndView;
     }
 
-    @RequestMapping("exportEvaluate/{teacherId}")
-    public ModelAndView exportEvaluate(@PathVariable String teacherId, HttpServletResponse response){
-        List<Evaluate> evaluateList = teacherService.exportEvaluateForm(teacherId);
-        if(evaluateList == null || evaluateList.size() == 0){
+    /*教师的评分相关
+    *test ip: http://localhost:8080/teacher/evalu?teacherId=100100&courseId=201603001&flag=4*/
+    @RequestMapping("/evalu")
+    public ModelAndView evaluation(Model model, String teacherId, String courseId, String flag) {
+        model.addAttribute("teacherId", teacherId);
+        model.addAttribute("courseId", courseId);
+        model.addAttribute("flag", flag);
+        List<EvaluateProblem> listEvaluateProblem = teacherService.getEvaluateProblem();
+        model.addAttribute("listEvaluateProblem", listEvaluateProblem);
+        return new ModelAndView("teacher/evalu");
+    }
+
+    @RequestMapping("/evaluScoreCaculate")
+    public ModelAndView evaluationScoreCaculate(@RequestParam("otherContent")String evaluateContent,
+                                                HttpServletRequest request, Model model,
+                                                @RequestParam("teacherId") String teacherId,
+                                                @RequestParam("courseId") String courseId,
+                                                @RequestParam("flag") String flag) {
+        double thisCourseScore = 0.0;
+        List<EvaluateProblem> listEvaluateProblem = teacherService.getEvaluateProblem();
+        for (EvaluateProblem evaluateProblem:listEvaluateProblem) {
+            thisCourseScore = thisCourseScore + Double.valueOf(request.getParameter(String.valueOf(evaluateProblem.getId())));
+        }
+        Evaluate evaluate = new Evaluate();
+        evaluate.setCourseId(courseId);
+        evaluate.setEvaluateContent(evaluateContent);
+        evaluate.setEvaluateScore(thisCourseScore);
+        evaluate.setFlag(flag);
+        evaluate.setFlagId(teacherId);
+        if (teacherService.evaluateTeacher(evaluate) == 0){
             return new ModelAndView("wrong");
+        }else {
+            return new ModelAndView("success");
         }
-
-        // 标题
-        String[] title = {"课程编号","课程评价","课程评分"};
-        // 唯一化excel表名
-        String fileName = teacherId+"评价表"+System.currentTimeMillis()+".xlsx";
-        String sheetName = "Sheet1";
-
-        // 构造excel主要内容
-        String[][] content = new String[evaluateList.size()][title.length];
-        for(int i = 0;i < evaluateList.size();i++){
-            Evaluate evaluate = evaluateList.get(i);
-            content[i][0] = evaluate.getCourseId();
-            content[i][1] = evaluate.getEvaluateContent();
-            content[i][2] = ""+evaluate.getEvaluateScore();
-        }
-
-        // 工具类获取Workbook对象
-        XSSFWorkbook wb = ExcelUitl.getXSSFWorkbook(sheetName,title,content);
-
-        // 下载-因为IO流，name需要额外转换
-        try{
-            fileName = new String(fileName.getBytes(),"ISO8859-1");
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-
-        // 下载-响应头设置
-        response.setContentType("application/octet-stream;charset=ISO8859-1");
-        response.setHeader("Content-Disposition", "attachment;filename="+ fileName);
-        response.addHeader("Pargam", "no-cache");
-        response.addHeader("Cache-Control", "no-cache");
-
-        // 直接在原页面下载，所以返回值为空即可
-        try{
-            OutputStream os = response.getOutputStream();
-            wb.write(os);
-            os.flush();
-            os.close();
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-
-        return null;
     }
 }
